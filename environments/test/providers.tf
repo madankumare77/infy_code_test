73be297b70844142caa451dcd7816957d9d8acb3
Madan Kumar Eligeti (Ernst and Young LLP) <v-maeligeti@microsoft.com>
2025-12-16 13:03:29 +0530

    variables added

--- environments/test/providers.tf
+++ environments/test/providers.tf
@@ -16,6 +16,10 @@ provider "azurerm" {
     }
 
   }
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+
   # subscription_id = "a0b36c09-679f-4dfb-829f-3b6685282dae"
   # tenant_id = "16b3c013-d300-468d-ac64-7eda0820b6d3"
 }
@@ -26,4 +30,13 @@ data "azurerm_client_config" "current" {}
 # Explicitly configure AzureAD provider using the current tenant
 provider "azuread" {
   tenant_id = data.azurerm_client_config.current.tenant_id
+}
+
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
 }
