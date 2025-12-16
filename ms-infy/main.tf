171bde3ebfab02a654d3365c56285e64a1cc8fc1
Madan Kumar Eligeti (Ernst and Young LLP) <v-maeligeti@microsoft.com>
2025-12-16 19:25:23 +0530

    vnet error

--- ms-infy/main.tf
+++ ms-infy/main.tf
@@ -99,7 +99,7 @@ module "vnet_created" {
 
   name                = each.value.name
   location            = each.value.location
-  resource_group_name = local.rg_name
+  #resource_group_name = local.rg_name
   address_space       = each.value.address_space
   dns_servers         = try(each.value.dns_servers, [])
   parent_id           = local.rg_id
