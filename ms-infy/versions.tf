524df8043868fb6b6c0268e31f9df7a126b5fa4a
Madan Kumar Eligeti (Ernst and Young LLP) <v-maeligeti@microsoft.com>
2025-12-17 13:19:35 +0530

    provider versions

--- ms-infy/versions.tf
+++ ms-infy/versions.tf
@@ -1,11 +1,12 @@
 
+
 terraform {
   required_version = ">= 1.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 3.116.0, < 4.0.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
